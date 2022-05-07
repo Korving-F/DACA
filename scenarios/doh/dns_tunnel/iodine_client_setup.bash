b4d4b487ce441a544930cb8ea364aca2f5318e70
Korving-F <korving.f@gmail.com>
2022-05-07 13:23:41 +0300

    Adds file transfer over DoH Tunnel scenario

--- scenarios/doh/dns_tunnel/iodine_client_setup.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/env bash
-echo "Installing IODINE"
-apt-get update
-DEBIAN_FRONTEND=noninteractive apt-get install -y git make build-essential pkg-config zlib1g-dev net-tools openssh-server iputils-ping netcat dnsutils libtext-lorem-perl sshpass asciinema
-cd /opt
-git clone https://github.com/yarrick/iodine.git
-cd /opt/iodine
-make
-make install
